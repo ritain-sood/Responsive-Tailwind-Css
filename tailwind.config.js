578bfc07a3552ea6445868f3112bd84e6dc08dd9
Ritain Sood <iamritainsood.04@gmail.com>
2025-02-02 23:43:46 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [tailwindcss()],
+}
+
+
+// // vite.config.js
+// import { defineConfig } from 'vite';
+// import tailwindcss from '@tailwindcss/vite';
+
+// export default defineConfig({
+//   plugins: [tailwindcss()],
+//   // Other Vite configurations...
+// });
+
